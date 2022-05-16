cff7a179950d3df3a6767fc79d90391f4d870022
Daniël Vink <daantjepietje@hotmail.com>
2022-05-16 17:20:18 +0200

    DV: fix attempt

--- thesis-unity/My project/Library/PackageCache/com.unity.purchasing@4.1.3/Runtime/Security/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-
-[assembly:InternalsVisibleTo("specs")]
-[assembly: InternalsVisibleTo("UnityEngine.Purchasing.EditorTests")]
-[assembly: InternalsVisibleTo("UnityEngine.Purchasing.RuntimeTests")]
